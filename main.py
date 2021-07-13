df98f8ff3b87f1015ee8a10c079f385031c0c2c7
hoobab <87278869+good-boi-huan@users.noreply.github.com>
2021-07-13 21:45:54 +0000

    while loop

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
 # Warm up
-first = input("What is your first name? ")
-last = input("What is your last name? ")
-print(f"{last} {first}")
+# first = input("What is your first name? ")
+# last = input("What is your last name? ")
+# print(f"Your name in reverse is {last} {first}")
+
+# Loops
+
+# Boolean operators
+# Comparision operators
+# < (less than)
+# > (greater than)
+# <= (less than or equal to)
+# >= (greater than or equal to)
+# == (equal to)
+# != (not equal to)
+# Let x = 3
+# x < 5 (less than) true
+# x > 5 (greater than) false
+# Let x = 5
+# x <= 5 (less than or equal to) true
+# x >= 5 (greater than or equal to) true
+# x == 5 (equal to) true
+# x != 5 (not equal to) false
+
+# Logical operators
+# let x = 5 and y = 6
+# and if x = 5 and if y = 5 false
+# or if x = 5 or if y = 5 true, if x = 6 or if y = 5 false
+# not if x is not 5 false, if x is not 4 true
+
+# While loop
+# initializing
+# counter = 0
+# # While loop takes in a boolean expression (T/F)\
+# # A while loop will only run if the boolean expression is true
+
+# while(counter < 5):
+#   print(counter + 1)
+#   counter = counter + 1
 
